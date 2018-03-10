a18530c01d8316839613d1de8ff026ee266c048a
chrisbialy <krzysztofbialy@o2.pl>
2018-03-10 13:59:03 +0100

    first commit

---
+++ public/thank_you.php
@@ -0,0 +1,21 @@
+<?php require_once("../resources/config.php"); ?>
+<!---- <?php //require_once("cart.php"); ?> ---->
+<?php include(TEMPLATE_FRONT . DS . "header.php");  ?>
+
+
+      
+
+    <!-- Page Content -->
+    <div class="container">
+
+
+        <h1 class="text-center">THANK YOU</h1>
+
+
+
+    </div>
+    <!-- /.container -->
+
+
+<?php include(TEMPLATE_FRONT . DS . "footer.php");  ?>
+
